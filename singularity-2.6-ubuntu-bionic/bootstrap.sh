29040e30a43b77e330138de5c095c794ec726362
GodloveD <davidgodlove@gmail.com>
2018-11-04 21:08:22 -0500

    fixing hard-coded log file error in 2.6

--- singularity-2.6-ubuntu-bionic/bootstrap.sh
+++ singularity-2.6-ubuntu-bionic/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+VER=2.6.0
+
 apt-get update && apt-get -y dist-upgrade
 apt-get -y install python dh-autoreconf build-essential libarchive-dev
 
-VER=2.6.0
 cd /tmp
 wget https://github.com/singularityware/singularity/releases/download/$VER/singularity-$VER.tar.gz
 tar xvf singularity-$VER.tar.gz
